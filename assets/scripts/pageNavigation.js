70cff3809ecf0be61ca412b2ce8dd89c7c0ff903
IvoFC <face9895@gmail.com>
2017-03-10 23:05:42 +0200

    SPA

--- assets/scripts/pageNavigation.js
+++ assets/scripts/pageNavigation.js
@@ -4,4 +4,14 @@
 
 (function () {
     var publishButton = document.querySelector(".publish");
+
+    publishButton.addEventListener('click', function (event) {
+        var mainDIv = document.querySelector('#homePage');
+        var publishPage = document.querySelector('#publishCars');
+
+        publishPage.style.display = 'block';
+        mainDIv.style.display = 'none';
+        event.preventDefault();
+    }, false)
+
 })();
