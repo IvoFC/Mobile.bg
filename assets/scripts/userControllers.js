0f5c44afa67a11b65a7f77fbc96767f12b237cfc
VladimirMilov <vladimirmilov96@gmail.com>
2017-03-10 20:19:43 +0200

    Added login/register form

--- assets/scripts/userControllers.js
+++ assets/scripts/userControllers.js
@@ -2,3 +2,10 @@
  * Created by ivo on 10.03.17.
  */
 
+(function(){
+    var homeLoginButton = document.getElementById("login-button");
+    homeLoginButton.addEventListener('click', function(event){
+        document.getElementById('login-register-popup').style.display = 'block';
+        event.preventDefault();
+    }, false)
+})();
