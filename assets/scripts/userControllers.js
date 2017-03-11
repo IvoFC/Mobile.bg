b7ccd068f29e229e5d5b580293e932a8612b1f68
VladimirMilov <vladimirmilov96@gmail.com>
2017-03-11 11:06:17 +0200

    Work on login-register form.

--- assets/scripts/userControllers.js
+++ assets/scripts/userControllers.js
@@ -6,6 +6,29 @@
     var homeLoginButton = document.getElementById("login-button");
     homeLoginButton.addEventListener('click', function(event){
         document.getElementById('login-register-popup').style.display = 'block';
+        document.getElementById("login-form").style.display = "block";
         event.preventDefault();
-    }, false)
+    }, false);
+
+    var homeRegisterButton = document.getElementById('register-button');
+    homeRegisterButton.addEventListener('click', function(){
+        document.getElementById('login-register-popup').style.display = 'block';
+        document.getElementById("register-form").style.display = "block";
+    }, false);
+
+// Popup login/registration form -->
+    var popupLoginButton = document.getElementById("showLoginForm");
+    popupLoginButton.addEventListener('click', function(){
+        document.getElementById("login-form").style.display = "block";
+        document.getElementById("register-form").style.display = "none";
+        console.log('login');
+    }, false);
+    
+    var popupRegistrationButton = document.getElementById("showRegistrationForm");
+    popupRegistrationButton.addEventListener('click', function(){
+        console.log('Register');
+        document.getElementById("register-form").style.display = "block";
+        document.getElementById("login-form").style.display = "none";
+    }, false);
+// End of popup login registration form
 })();
