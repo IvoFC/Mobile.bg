484b07af72f5319ad6d2e3d95abf07feb7fab860
IvoFC <face9895@gmail.com>
2017-03-12 15:52:39 +0200

    Add car

--- assets/scripts/carService.js
+++ assets/scripts/carService.js
@@ -1,11 +1,11 @@
-function Car(brand, model, price, engine) {
+function Car(brand, model, price, engine, extras) {
     this.brand = brand;
     this.model = model;
     this.price = price;
     this.engine = engine;
-
+    this.extras = extras;
     this.images = [];
-    this.extras = [];
+
 }
 
 var carManager = (function () {
@@ -13,6 +13,8 @@ var carManager = (function () {
     var cars = [new Car('BMW', 'E36', 1500, 'gas')];
 
     return {
+        models: cars,
+
         addCar: function (car) {
             if (car instanceof Car) {
                 cars.push(car);
