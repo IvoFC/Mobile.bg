002e04951eed7d5a4c2ccaf20ad0a3862b7ae8a8
IvoFC <face9895@gmail.com>
2017-03-12 11:28:56 +0200

    Seach options

--- assets/scripts/pageNavigation.js
+++ assets/scripts/pageNavigation.js
@@ -1,17 +1,55 @@
-/**
- * Created by ivo on 10.03.17.
- */
-
 (function () {
     var publishButton = document.querySelector(".publish");
+    var searchButton = document.querySelector(".searching");
+    var homeButton = document.querySelector('.homeBut');
 
     publishButton.addEventListener('click', function (event) {
         var mainDIv = document.querySelector('#homePage');
         var publishPage = document.querySelector('#publishCars');
+        var searchingPage = document.querySelector('#searching');
 
+        publishButton.className = 'active';
+        searchButton.className = 'searching';
+        homeButton.className = 'homeBut';
+
+        searchingPage.style.display = 'none';
         publishPage.style.display = 'block';
         mainDIv.style.display = 'none';
+
         event.preventDefault();
-    }, false)
+    }, false);
+
+    searchButton.addEventListener('click', function (event) {
+        var mainDIv = document.querySelector('#homePage');
+        var publishPage = document.querySelector('#publishCars');
+        var searchingPage = document.querySelector('#searching');
+
+        searchButton.className = 'active';
+        publishButton.className = "publish";
+        homeButton.className = "homeBut";
+
+        searchingPage.style.display = 'block';
+        publishPage.style.display = 'none';
+        mainDIv.style.display = 'none';
+
+        event.preventDefault();
+    }, false);
+
+    homeButton.addEventListener('click', function (event) {
+        var mainDIv = document.querySelector('#homePage');
+        var publishPage = document.querySelector('#publishCars');
+        var searchingPage = document.querySelector('#searching');
+
+        homeButton.className = 'active';
+        searchButton.className = 'searching';
+        publishButton.className = "publish";
+
+
+        searchingPage.style.display = 'none';
+        publishPage.style.display = 'none';
+        mainDIv.style.display = 'block';
+
+        event.preventDefault();
+    }, false);
 
 })();
