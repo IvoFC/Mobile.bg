7a0982f45888e848d43a174aeb954f5297551204
stanislav-velichkov <stanislav.v.velichkov@gmail.com>
2017-03-15 21:56:48 +0200

    Changes in Publish Preview

--- assets/scripts/pageNavigation.js
+++ assets/scripts/pageNavigation.js
@@ -3,11 +3,17 @@
     var searchButton = document.querySelector(".searching");
     var homeButton = document.querySelector('.homeBut');
     var myMobileButton = document.getElementById('navMyMobile');
+    var viewPublicationButton = document.getElementById('viewPublicationButton');
+    var backToPublish = document.getElementById('backToPublishText');
+
+
 
     publishButton.addEventListener('click', function (event) {
         var mainDIv = document.querySelector('#homePage');
         var publishPage = document.querySelector('#publishCars');
         var searchingPage = document.querySelector('#searching');
+        var publishPreview = document.querySelector('#viewPublication');
+        var previewSelections = document.querySelector('#previewSelections');
 
         publishButton.className = 'active';
         searchButton.className = 'searching';
@@ -16,6 +22,69 @@
         searchingPage.style.display = 'none';
         publishPage.style.display = 'block';
         mainDIv.style.display = 'none';
+        publishPreview.style.display = 'none';
+        previewSelections.innerHTML = '';
+
+        event.preventDefault();
+    }, false);
+
+    viewPublicationButton.addEventListener('click', function (event) {
+
+        var mainDIv = document.querySelector('#homePage');
+        var publishPage = document.querySelector('#publishCars');
+        var searchingPage = document.querySelector('#searching');
+        var publishPreview = document.querySelector('#viewPublication');
+        
+
+        publishButton.className = 'active';
+        searchButton.className = 'searching';
+        homeButton.className = 'homeBut';
+
+        searchingPage.style.display = 'none';
+        publishPage.style.display = 'none';
+        mainDIv.style.display = 'none';
+        publishPreview.style.display = 'block';
+
+        //Preview Publication:
+
+        //radios
+        var radioButtons = document.querySelectorAll('input[type="radio"]:checked');
+        for (var index = 0; index < radioButtons.length; index++) {
+            document.getElementById("previewSelections").innerHTML += radioButtons[index].value + "<br/>";
+        }
+
+        
+
+        //checkboxes
+        var checkBoxes = document.querySelectorAll('#publish input[type="checkbox"]:checked');
+        
+        document.getElementById("previewSelections").innerHTML += "<br/><br/> Екстри: </br>";
+        for (var index = 0; index < checkBoxes.length; index++) {
+            document.getElementById("previewSelections").innerHTML += checkBoxes[index].value + "<br/>";
+        }
+
+
+        event.preventDefault();
+    }, false);
+
+    backToPublish.addEventListener('click', function (event) {
+
+        var mainDIv = document.querySelector('#homePage');
+        var publishPage = document.querySelector('#publishCars');
+        var searchingPage = document.querySelector('#searching');
+        var publishPreview = document.querySelector('#viewPublication');
+        var previewSelections = document.querySelector('#previewSelections');
+        
+
+        publishButton.className = 'active';
+        searchButton.className = 'searching';
+        homeButton.className = 'homeBut';
+
+        searchingPage.style.display = 'none';
+        publishPage.style.display = 'block';
+        mainDIv.style.display = 'none';
+        publishPreview.style.display = 'none';
+        previewSelections.innerHTML = '';
 
         event.preventDefault();
     }, false);
@@ -24,6 +93,7 @@
         var mainDIv = document.querySelector('#homePage');
         var publishPage = document.querySelector('#publishCars');
         var searchingPage = document.querySelector('#searching');
+        var publishPreview = document.querySelector('#viewPublication');
 
         searchButton.className = 'active';
         publishButton.className = "publish";
@@ -32,6 +102,7 @@
         searchingPage.style.display = 'block';
         publishPage.style.display = 'none';
         mainDIv.style.display = 'none';
+        publishPreview.style.display = 'none';
 
         event.preventDefault();
     }, false);
@@ -40,6 +111,8 @@
         var mainDIv = document.querySelector('#homePage');
         var publishPage = document.querySelector('#publishCars');
         var searchingPage = document.querySelector('#searching');
+        var publishPreview = document.querySelector('#viewPublication');
+        var previewSelections = document.querySelector('#previewSelections');
 
         homeButton.className = 'active';
         searchButton.className = 'searching';
@@ -49,6 +122,8 @@
         searchingPage.style.display = 'none';
         publishPage.style.display = 'none';
         mainDIv.style.display = 'block';
+        publishPreview.style.display = 'none';
+        previewSelections.innerHTML = '';
 
         event.preventDefault();
     }, false);
@@ -58,11 +133,13 @@
         var publishPage = document.querySelector('#publishCars');
         var searchingPage = document.querySelector('#searching');
         var myMobilePage = document.querySelector('#my-mobile');
+        var publishPreview = document.querySelector('#viewPublication');
 
         searchingPage.style.display = 'none';
         publishPage.style.display = 'none';
         mainDIv.style.display = 'none';
         myMobilePage.style.display = 'block';
+        publishPreview.style.display = 'none';
 
         event.preventDefault();
     }, false);
