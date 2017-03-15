e66e261ef5c9829f238e02a4dc44c6ddc8044488
stanislav-velichkov <stanislav.v.velichkov@gmail.com>
2017-03-16 00:09:28 +0200

    Created Separate Preview script

--- assets/scripts/pageNavigation.js
+++ assets/scripts/pageNavigation.js
@@ -45,41 +45,6 @@
         mainDIv.style.display = 'none';
         publishPreview.style.display = 'block';
 
-        //Preview Publication:
-
-        //radios
-        var radioButtons = document.querySelectorAll('input[type="radio"]:checked');
-        for (var index = 0; index < radioButtons.length; index++) {
-            document.getElementById("previewSelections").innerHTML += radioButtons[index].value + "<br/><br/>";
-        }
-
-        //selects
-        var inputSelects = document.querySelectorAll('#publish select');
-        for (var index = 0; index < inputSelects.length; index++) {
-            if(inputSelects[index].value != '') {
-                document.getElementById("previewSelections").innerHTML += inputSelects[index].name + ": <br/>";
-                document.getElementById("previewSelections").innerHTML += inputSelects[index].value + "<br/><br/>";
-            }    
-        }
-
-        //input texts
-        var inputTexts = document.querySelectorAll('#publish input[type="text"]');
-        for (var index = 0; index < inputTexts.length; index++) {
-            if(inputTexts[index].value != '') {
-                document.getElementById("previewSelections").innerHTML += inputTexts[index].name + ": <br/>";
-                document.getElementById("previewSelections").innerHTML += inputTexts[index].value + "<br/><br/>";
-            }    
-        }
-
-        //checkboxes
-        var checkBoxes = document.querySelectorAll('#publish input[type="checkbox"]:checked');
-        
-        document.getElementById("previewSelections").innerHTML += "<br/><br/> Екстри: </br>";
-        for (var index = 0; index < checkBoxes.length; index++) {
-            document.getElementById("previewSelections").innerHTML += checkBoxes[index].value + ", ";
-        }
-
-
         event.preventDefault();
     }, false);
 
